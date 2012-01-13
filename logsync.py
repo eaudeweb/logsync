d2c6eaa59f2f233c67470e0f99b804631f30cb0e
Alex Morega <alex@grep.ro>
2012-01-13 11:50:13 +0200

    log commands

--- logsync.py
+++ logsync.py
@@ -1,4 +1,9 @@
 import json
+import logging
+
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
 
 class LogSyncer(object):
@@ -6,8 +11,11 @@ class LogSyncer(object):
     def __init__(self, **config):
         self.config = config
 
+    def _ssh(self, args):
+        log.debug('ssh to %s: %r', self.config['host'], args)
+
     def list_remote_files(self):
-        print 'list'
+        self._ssh(['ls', self.config['remote-path']])
 
     def sync(self):
         self.list_remote_files()
@@ -15,5 +23,6 @@ class LogSyncer(object):
 
 if __name__ == '__main__':
     import sys
+    logging.basicConfig(loglevel=logging.DEBUG)
     with open(sys.argv[1]) as f:
         LogSyncer(**json.load(f)).sync()
